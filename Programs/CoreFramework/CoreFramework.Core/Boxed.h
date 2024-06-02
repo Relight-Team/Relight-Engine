c17f0b96024d20de1be47eaf9ed10aed3518e677
a <ethan.boi.dev.615@gmail.com>
2024-06-02 17:45:36 -0500

    added Boxed.h

---
+++ Programs/CoreFramework/CoreFramework.Core/Boxed.h
@@ -0,0 +1,21 @@
+// Owned by Relight Engine 2024
+
+#include <iostream>
+
+using namespace std;
+
+template<typename T>
+
+
+
+public class Boxed<t>
+{
+    public:
+        T var;
+
+
+        public Boxed(T Value)
+        {
+            var = Value;
+        }
+}
