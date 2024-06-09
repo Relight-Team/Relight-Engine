f9ef2edd95b75720273b7f835ccbf796ab9effde
a <ethan.boi.dev.615@gmail.com>
2024-06-08 21:50:49 -0500

    Added CoreFramework.Core.h

---
+++ Programs/CoreFramework/CoreFramework.Core/CoreFramework.Core.h
@@ -0,0 +1,9 @@
+// Owned by Relight Engine 2024
+
+
+// this imports everything from CoreFramework.Core, so just include this header file to include everything
+
+#include "ArrayExtensions.h"
+#include "AssemblyUtils.h"
+#include "AsyncEvent.h"
+#include "Boxed.h"
