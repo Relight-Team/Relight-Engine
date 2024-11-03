84d7767516530f88bf653c2fc067ac2d1bcc69fe
EthanBoiLol <ethan.boi.dev.615@gmail.com>
2024-11-02 20:07:38 -0500

    started working on config files

--- Runtime/Core/Src/Etc/Config.cpp
+++ Runtime/Core/Src/Etc/Config.cpp
@@ -1,6 +1,30 @@
 #include "Log.h"
+#include "Config_Internal.cpp"
+#include <iostream>
 
 
-// Note, getting values will look like this:
+// Note, params for getting values in configs
+// ------------------------------------------------
+// PClass: the config class the value is stored in
+// Value: the value name from the class to get
+// Store: The value to store the config
+// File: The file location that stores the configs
 
-// GetBool(Class Path, Config Value, Value to store, File of config)
+class Config
+{
+
+    public:
+
+        // Get values
+
+        void GetString(std::string PClass, std::string Value, std::string& Store, std::string File)
+        {
+            // TODO: Finish me!
+        }
+
+
+    private:
+
+    std::string Text;
+
+};
--- Runtime/Core/Src/Etc/Config_Internal.cpp
+++ Runtime/Core/Src/Etc/Config_Internal.cpp
@@ -6,7 +6,66 @@
 
 //This is an internal file for config.cpp, this does NOT need to be public for the CORE_API
 
-void ReadInternal(std::string config file)
+std::string ReadLineInternal(std::stirng ConfigFile, int Line)
 {
-    //TODO: Everything...
+   std::ifstream file(ConfigFile);
+
+   return std::getline(ConfigFile, Line);
+}
+
+int FileLengthInternal(std::string ConfigFile)
+{
+    std::ifstream file(ConfigFile);
+
+    std::string Line;
+
+    int i = 0;
+
+    while(std::getline(ConfigFile, Line))
+    {
+        i++;
+    }
+
+    return i;
+}
+
+
+// This should detect if the beginning text contains context, no matter how big context is
+bool ContainsInternal(std::string Line, std::string Context)
+{
+    int LineSize = Line.size();
+    int ContextSize = Context.size();
+
+    // if Line is smaller than context, no need to return true
+    if(LineSize < ContextSize)
+    {
+        return false;
+    }
+
+    for(int i = 0; i < ContextSize; i++)
+    {
+        if(!(Context[i] == Line[i]))
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+std::string ReadInternal(std::string ConfigFile)
+{
+    std::string Text;
+
+    for(int i = 0; i > FileLengthInternal(ConfigFile); i++)
+    {
+        // ignore ; comments
+
+        if(!(ContainsInternal(ReadLineInternal(ConfigFile, i), ";")))
+        {
+            Text += ReadLineInternal(ConfigFile, i);
+        }
+
+    }
+
+    return Text;
 }
