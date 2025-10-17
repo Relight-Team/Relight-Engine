44889a0250208fd82692ab276927fbef5db06ce0
EthanBoiLol <ethan.boi.dev.615@gmail.com>
2025-10-17 16:35:05 -0500

    Created String and Core Platform

---
+++ Runtime/Core/Src/Containers/String.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "Array.h"
+
+// String system
+
+// Handles Relight's strings
+
+class String
+{
+    public:
+
+    Array<char> CharArr;
+};
---
+++ Runtime/Core/Src/Platform.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#ifdef RE_PLATFORM_LINUX
+#include "Linux/"
