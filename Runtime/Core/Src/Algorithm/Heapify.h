c323e653a5dec1936e6be1a0534bc0cb8cc3a2b3
EthanBoiLol <ethan.boi.dev.615@gmail.com>
2025-11-07 22:11:10 -0600

    Fixed bug in heapify

--- Runtime/Core/Src/Algorithm/Heapify.h
+++ Runtime/Core/Src/Algorithm/Heapify.h
@@ -1,11 +1,21 @@
 // Relight Engine's heapify function
 
+#include <iostream>
+
 template <typename ArrayType>
 void Heapify(ArrayType& Arr, int HeapSize, int Root)
 {
+
+
     int Largest = Root;
     int Left = 2 * Root + 1;
     int Right = 2 * Root + 2;
+
+        std::cout << "Root=" << Root
+          << " Left=" << Left
+          << " Right=" << Right
+          << " HeapSize=" << HeapSize
+          << std::endl;
 
     // If left is larger than root
     if(Left < HeapSize && Arr[Left] > Arr[Largest])
@@ -21,7 +31,7 @@ void Heapify(ArrayType& Arr, int HeapSize, int Root)
 
     if(Largest != Root)
     {
-        Arr.Swap(Arr[Root], Arr[Largest]);
+        Arr.Swap(Root, Largest);
         Heapify(Arr, HeapSize, Largest);
     }
 }
