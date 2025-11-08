0a56bc4c76c56799c3b76f63e063f85af2b99807
EthanBoiLol <ethan.boi.dev.615@gmail.com>
2025-11-07 22:12:31 -0600

    Removed old debug from Heapify.h

--- Runtime/Core/Src/Algorithm/Heapify.h
+++ Runtime/Core/Src/Algorithm/Heapify.h
@@ -1,7 +1,5 @@
 // Relight Engine's heapify function
 
-#include <iostream>
-
 template <typename ArrayType>
 void Heapify(ArrayType& Arr, int HeapSize, int Root)
 {
@@ -10,12 +8,6 @@ void Heapify(ArrayType& Arr, int HeapSize, int Root)
     int Largest = Root;
     int Left = 2 * Root + 1;
     int Right = 2 * Root + 2;
-
-        std::cout << "Root=" << Root
-          << " Left=" << Left
-          << " Right=" << Right
-          << " HeapSize=" << HeapSize
-          << std::endl;
 
     // If left is larger than root
     if(Left < HeapSize && Arr[Left] > Arr[Largest])
