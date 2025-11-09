a0296e05f2ad73e3b3527e797eb50682e9709d7d
EthanBoiLol <ethan.boi.dev.615@gmail.com>
2025-11-08 21:07:22 -0600

    Added HeapSort.h

---
+++ Runtime/Core/Src/Algorithm/HeapSort.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "Algorithm/Heapify.h"
+// Sort's an array using heap
+
+
+template <typename ArrayType>
+void HeapSort(ArrayType& Arr)
+{
+    int Length = Arr.Length();
+
+    for(int I = Length / 2 - 1; I >= 0; I--)
+    {
+        Heapify(Arr, Length, I);
+    }
+
+    for(int I = Length; I > 0; I--)
+    {
+        Arr.Swap(0, I);
+        Heapify(Arr, I, 0);
+    }
+}
