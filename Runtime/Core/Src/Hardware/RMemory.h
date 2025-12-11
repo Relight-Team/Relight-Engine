e6748287cab8423cacbccf02d973fbe9a9badca1
EthanBoiLol <ethan.boi.dev.615@gmail.com>
2025-12-11 00:12:44 -0600

    For now, we will simply use the standard library, but this will be replaced later

--- Runtime/Core/Src/Hardware/RMemory.h
+++ Runtime/Core/Src/Hardware/RMemory.h
@@ -5,8 +5,44 @@
 // Please replace this later with custom version!
 #include <iostream>
 #include <cstdlib>
+#include <string.h>
 
 struct RMemory
 {
+
+    static void* Malloc(size_t Size, int Alignment = 0)
+    {
+        return malloc(Size);
+    }
+
+    static void* Realloc(void* Ptr, size_t Size, int Alignment = 0)
+    {
+        return realloc(Ptr, Size);
+    }
+
+    static void Free(void* Ptr)
+    {
+        free(Ptr);
+    }
+
+    static void* Memmove(void* Dest, void* Src, size_t Size)
+    {
+        return memmove(Dest, Src, Size);
+    }
+
+    static int Memcmp(void* A, void* B, size_t Size)
+    {
+        return memcmp(A, B, Size);
+    }
+
+    static void* Memset(void* A, int C, size_t Size)
+    {
+        return memset(A, C, Size);
+    }
+
+    static void* Memcpy(void* Dest, void* Src, size_t Size)
+    {
+        return memcpy(Dest, Src, Size);
+    }
 
 };
