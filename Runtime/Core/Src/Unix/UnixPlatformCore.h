4de37cdcd5cbe23e0af95c3c94b65b90dcf06d4f
EthanBoiLol <ethan.boi.dev.615@gmail.com>
2026-03-06 15:11:19 -0600

    added type defines for each unique integers

--- Runtime/Core/Src/Unix/UnixPlatformCore.h
+++ Runtime/Core/Src/Unix/UnixPlatformCore.h
@@ -1,3 +1,12 @@
 #pragma once
 #include "UnixErrorHandling.h"
 typedef char16_t UTF16;
+
+typedef signed char int8;
+typedef unsigned char uint8;
+typedef signed short int int16;
+typedef unsigned short int uint16;
+typedef signed int int32;
+typedef unsigned int uint32;
+typedef signed long long int64;
+typedef unsigned long long uint64;
