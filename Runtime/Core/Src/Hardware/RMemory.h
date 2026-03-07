fb1a977ca3d251d47da15d37f044f4f70398fe3e
EthanBoiLol <ethan.boi.dev.615@gmail.com>
2026-03-06 21:40:59 -0600

    Updated integer naming, added memzero()

--- Runtime/Core/Src/Hardware/RMemory.h
+++ Runtime/Core/Src/Hardware/RMemory.h
@@ -6,16 +6,17 @@
 #include <iostream>
 #include <cstdlib>
 #include <string.h>
+#include "PlatformCore.h"
 
 struct RMemory
 {
 
-    static void* Malloc(size_t Size, int Alignment = 0)
+    static void* Malloc(size_t Size, int32 Alignment = 0)
     {
         return malloc(Size);
     }
 
-    static void* Realloc(void* Ptr, size_t Size, int Alignment = 0)
+    static void* Realloc(void* Ptr, size_t Size, int32 Alignment = 0)
     {
         return realloc(Ptr, Size);
     }
@@ -30,12 +31,12 @@ struct RMemory
         return memmove(Dest, Src, Size);
     }
 
-    static int Memcmp(void* A, void* B, size_t Size)
+    static int32 Memcmp(void* A, void* B, size_t Size)
     {
         return memcmp(A, B, Size);
     }
 
-    static void* Memset(void* A, int C, size_t Size)
+    static void* Memset(void* A, int32 C, size_t Size)
     {
         return memset(A, C, Size);
     }
@@ -43,6 +44,11 @@ struct RMemory
     static void* Memcpy(void* Dest, void* Src, size_t Size)
     {
         return memcpy(Dest, Src, Size);
+    }
+
+    static void* Memzero(void* A, size_t Size)
+    {
+        return Memset(A, 0, Size);
     }
 
 };
